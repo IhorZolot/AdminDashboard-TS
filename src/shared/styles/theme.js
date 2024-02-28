8247b7356cd88207cbbaf676ea9fc5e180ce805a
IhorZolot <fedonn@ukr.net>
2024-02-28 23:28:55 +0200

    add color, theme

---
+++ src/shared/styles/theme.js
@@ -0,0 +1,23 @@
+export const theme = {
+  colors: {
+    bgColor: '#f7f8fa',
+    tabColor: '#e7f1ed',
+    styleColor: '#59b17a',
+    mainText: ' #1d1e21',
+  },
+  status: {
+    cansel: '#e85050',
+    bgCansel: 'rgba(232, 80, 80, 0.1)',
+    proces: '#70a6e8',
+    bgProces: 'rgba(112, 166, 232, 0.1)',
+    pending: '#f79042',
+    bgPending: 'rgba(247, 144, 66, 0.1)',
+    confim: '#8059e4',
+    bgConfim: 'rgba(128, 89, 228, 0.1)',
+    complet: '#59b17a',
+    bgComplet: ' rgba(89, 177, 122, 0.1)',
+  },
+  button: {
+    cansel: 'rgba(29, 30, 33, 0.1)',
+  },
+};

00d65591dcd63b7cfee348f4cb27e6072ce0b37f
IhorZolot <fedonn@ukr.net>
2024-03-05 18:45:39 +0200

    update global styles modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-// import React from 'react';
-
-import { Global } from './shared/styles/Global';
-
 const App = () => {
-  return (
-    <div>
-      <Global />
-      App
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;

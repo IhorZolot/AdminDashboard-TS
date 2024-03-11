0039736fc3c9d6e9acef59b347c3c1a7165c6b3f
IhorZolot <fedonn@ukr.net>
2024-03-11 10:41:08 +0200

    add Navigation and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,32 @@
+import { NavLink, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import DasboardPage from './pages/DasboardPage/DasboardPage';
+import AllOrdersPage from './pages/AllOrdersPage/AllOrdersPage';
+import AllProductsPage from './pages/AllProductsPage/AllProductsPage';
+import AllSuppliersPage from './pages/AllSuppliersPage/AllSuppliersPage';
+import CustomersDataPage from './pages/CustomersDataPage/CustomersDataPage';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <nav>
+        <NavLink to="/">D</NavLink>
+        <NavLink to="/dashboard">H</NavLink>
+        <NavLink to="/orders">M</NavLink>
+        <NavLink to="/products">R</NavLink>
+        <NavLink to="/suppliers">Q</NavLink>
+        <NavLink to="/customers">Y</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dashboard" element={<DasboardPage />} />
+        <Route path="/orders" element={<AllOrdersPage />} />
+        <Route path="/products" element={<AllProductsPage />} />
+        <Route path="/suppliers" element={<AllSuppliersPage />} />
+        <Route path="/customers" element={<CustomersDataPage />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/DasboardPage/DasboardPage.jsx
@@ -0,0 +1,11 @@
+import Dasboard from '../../modules/dasboard';
+
+const DasboardPage = () => {
+  return (
+    <div>
+      <Dasboard />
+    </div>
+  );
+};
+
+export default DasboardPage;

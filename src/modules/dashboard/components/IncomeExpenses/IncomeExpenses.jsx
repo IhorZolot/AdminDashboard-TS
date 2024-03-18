5636fb2db4438ba436ca84385f5dd16daaff9576
IhorZolot <fedonn@ukr.net>
2024-03-18 22:11:28 +0200

    add component IncomeExpenses

--- src/modules/dashboard/components/IncomeExpenses/IncomeExpenses.jsx
+++ src/modules/dashboard/components/IncomeExpenses/IncomeExpenses.jsx
@@ -1,5 +1,26 @@
+import styles from './IncomeExpenses.module.scss';
 const IncomeExpenses = () => {
-  return <div>IncomeExpenses</div>;
+  return (
+    <div className={styles.sectionExpenses}>
+      <h2 className={styles.titleExpenses}>Income/Expenses</h2>
+      <table className={styles.table}>
+        <thead className={styles.theadExpenses}>
+          <tr>
+            <th>Today</th>
+          </tr>
+        </thead>
+        <tbody className={styles.dataExpenses}>
+          <tr>
+            <td>
+              <button className={styles.button}>Income</button>
+            </td>
+            <td>Cruip.com Market Ltd 70 Wilson St London</td>
+            <td>+249.88</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default IncomeExpenses;

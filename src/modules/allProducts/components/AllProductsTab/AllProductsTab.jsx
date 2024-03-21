dd1c2875708dd9b159b3c5cb07019b7d14d7d589
IhorZolot <fedonn@ukr.net>
2024-03-21 08:21:30 +0200

    update little bags

--- src/modules/allProducts/components/AllProductsTab/AllProductsTab.jsx
+++ src/modules/allProducts/components/AllProductsTab/AllProductsTab.jsx
@@ -3,9 +3,6 @@ import ActionsBottom from '../ActionsBottom/ActionsBottom';
 import styles from './AllProductsTab.module.scss';
 
 const AllProductsTab = ({ onOpen }) => {
-  const handleClick = actionType => {
-    onOpen(actionType);
-  };
   const headers = [
     'Product Info',
     'Category',
@@ -22,8 +19,8 @@ const AllProductsTab = ({ onOpen }) => {
       'Square',
       '89.66',
       <div key="uniqueKey" className={styles.action}>
-        <ActionsBottom onClick={handleClick} actionType="edit" />
-        <ActionsBottom onClick={handleClick} actionType="delete" />
+        <ActionsBottom onClick={() => onOpen('edit')} actionType="edit" />
+        <ActionsBottom actionType="delete" />
       </div>,
     ],
   ];

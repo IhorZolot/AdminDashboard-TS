305e29fbe11fbf3cfec46963e98dd4e5823b0e84
IhorZolot <fedonn@ukr.net>
2024-03-22 21:49:34 +0200

    Add Formik and Yup validations

--- src/modules/login/components/loginForm/LoginForm.jsx
+++ src/modules/login/components/loginForm/LoginForm.jsx
@@ -1,26 +1,52 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import styles from './LoginForm.module.scss';
+import * as yup from 'yup';
+
+const userSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(6).max(12).required(),
+});
+
+const initialValues = {
+  email: '',
+  password: '',
+};
 
 const LoginForm = () => {
+  const handleSubmit = (values, { resetForm }) => {
+    console.log(values);
+    resetForm();
+  };
   return (
-    <form className={styles.loginForm}>
-      <label className={styles.loginLable}>
-        <input
-          className={styles.loginInput}
-          type="email"
-          name="email"
-          placeholder="Email address"
-        />
-      </label>
-      <label className={styles.loginLable}>
-        <input
-          className={styles.loginInput}
-          type="text"
-          name="password"
-          placeholder="Password"
-        />
-      </label>
-      <button className={styles.loginButton}>Log in</button>
-    </form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={userSchema}
+      onSubmit={handleSubmit}
+    >
+      <Form className={styles.loginForm}>
+        <label className={styles.loginLable}>
+          <Field
+            className={styles.loginInput}
+            type="email"
+            name="email"
+            placeholder="Email address"
+          />
+          <ErrorMessage name="email" />
+        </label>
+        <label className={styles.loginLable}>
+          <Field
+            className={styles.loginInput}
+            type="password"
+            name="password"
+            placeholder="Password"
+          />
+          <ErrorMessage name="password" />
+        </label>
+        <button type="submit" className={styles.loginButton}>
+          Log in
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
